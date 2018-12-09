c99d9048a9b28dafabe9290f1a3f915dcbad1012
Jonathan Müller <JoyeMueller@aol.com>
2018-12-09 19:20:05 +0100

    Added Pick Up
    You can now press E to pick up the object, that you are looking at

--- Assets/Scripts/PlayerController/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController/Scripts/PlayerController.cs
@@ -8,15 +8,20 @@ public class PlayerController : MonoBehaviour {
 	public float jumpSpeed = 5.0f;
 	float hInput, vInput;
 
-	private float distToGround;
-	private Rigidbody rb;
+    private Rigidbody rb;
+    private Ray ray;
+    private RaycastHit hit;
+    private GameObject load;
+    private float distToGround;
+    private bool loaded;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		Cursor.lockState = CursorLockMode.Locked;
 		rb = GetComponent<Rigidbody>();
 
 		distToGround = gameObject.GetComponent<Collider>().bounds.extents.y;
+        loaded = false;
 	}
 	
 	// Update is called once per frame
@@ -26,8 +31,13 @@ public class PlayerController : MonoBehaviour {
 
 		hInput *= Time.deltaTime;
 		vInput *= Time.deltaTime;
-
-		if (Input.GetButtonDown("Jump") && isGrounded()) {
+        #region loading
+        if (loaded && load != null)
+        {
+            load.transform.position = transform.GetChild(0).transform.position;
+        }
+        #endregion  // This fixes the target to the player every frame
+        if (Input.GetButtonDown("Jump") && isGrounded()) {
 			rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
 		}
 
@@ -36,9 +46,42 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKeyDown("escape")) {
 			Cursor.lockState = CursorLockMode.None;
 		}
-	}
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                if(loaded)
+                {
+                    unloadTarget();
+                }else
+                {
+                    loadTarget(hit.collider.gameObject);
+                }
+            }      
+        }
+    }
 
 	private bool isGrounded() {
    		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
  	}
+
+    private void loadTarget(GameObject target)
+    {
+        loaded = true;
+        load = target;
+        load.GetComponent<Collider>().isTrigger = true;
+        load.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    private void unloadTarget()
+    {
+        load.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+       
+        loaded = false;
+        load = null;
+    }
 }
