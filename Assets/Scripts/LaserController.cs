d4e516a60f052b45c261be679c8944ccf8248185
Felix K <herubrin@gmail.com>
2019-01-21 17:44:58 +0100

    Added lasers
    Lasers have variable length
    TODO: update laser length on the fly

---
+++ Assets/Scripts/LaserController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserController : MonoBehaviour {
+
+	public GameObject LaserBeam;
+
+	private GameObject laserChild, laserBeam;
+	private float maxLaserLength = 100f;
+	private float lengthFix = 5.5f;
+	private RaycastHit foundWall;
+	void Start () {
+		// create laser beam as child
+		laserBeam = (GameObject) Instantiate(LaserBeam, transform.position, transform.rotation);
+
+		// get distance to next wall and scale laser accordingly
+		Ray distanceRay = new Ray(transform.position, new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z));
+		if (Physics.Raycast(distanceRay, out foundWall)) {
+			laserBeam.transform.localScale = new Vector3(1, 1, -foundWall.distance * lengthFix);
+		}
+	}
+}
