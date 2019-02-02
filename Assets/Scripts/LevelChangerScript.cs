32f9155d8bf72bb09e40dd4f1efdb5da8fcea7ed
Jonathan Müller <JoyeMueller@aol.com>
2019-02-02 17:37:44 +0100

    Almost finished first tutorial, started working on the startscreen

---
+++ Assets/Scripts/LevelChangerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChangerScript : MonoBehaviour {
+
+    public static void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+
+    public static void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");            
+    }
+}
